a8f6acc400c12427b7a533ab26a1dcdfb53b42b8
maximLomako <maxim.lomako@icloud.com>
2020-10-14 17:28:57 +0300

    feat: add uidd library

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -1,23 +1,22 @@
 import React, {useState} from "react";
 import GreetingContainer from "./GreetingContainer";
+import {v1} from "uuid";
 
 // types
 export type UserType = {
-    _id: number
+    _id: string
     name: string
 }
 
 // уровень работы с глобальными данными
 function HW3() {
     const [users, setUsers] = useState<Array<UserType>>([
-      {_id: 0, name: 'Petya'},
-      {_id: 1, name: 'Vasya'},
-      {_id: 2, name: 'Kolya'},
-      {_id: 3, name: 'Sveta'}
     ]);
+  console.log(users);
 
-    const addUserCallback = (name: string) => {
-      const newUser = {_id: 4, name: name};
+
+  const addUserCallback = (name: string) => {
+      const newUser = {_id: v1(), name: name};
       const newUsers = [newUser, ...users];
         setUsers(newUsers);
     }
