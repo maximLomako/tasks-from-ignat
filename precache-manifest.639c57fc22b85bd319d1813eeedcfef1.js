1d8184920916b24aab0d166ba5120061ab9ac420
maximLomako <maxim.lomako@icloud.com>
2020-11-20 16:07:07 +0300

    Updates

---
+++ precache-manifest.639c57fc22b85bd319d1813eeedcfef1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5671b5dc861f5ce033cce13250744d9",
+    "url": "/tasks-from-ignat/index.html"
+  },
+  {
+    "revision": "e2877dd893d027657b79",
+    "url": "/tasks-from-ignat/static/css/main.5dd0c67e.chunk.css"
+  },
+  {
+    "revision": "6b4fd3ea3dd3cee7c51f",
+    "url": "/tasks-from-ignat/static/js/2.51806501.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tasks-from-ignat/static/js/2.51806501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2877dd893d027657b79",
+    "url": "/tasks-from-ignat/static/js/main.47a31804.chunk.js"
+  },
+  {
+    "revision": "8974a57968ec0002eb36",
+    "url": "/tasks-from-ignat/static/js/runtime-main.5f9320e5.js"
+  }
+]);
